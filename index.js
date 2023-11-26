5881c549b543d545667b5e80a0f8dd884f7fdb8a
rabby4 <mdrabbybd4747@gmail.com>
2023-11-27 05:07:19 +0600

    install jwt and create get api and patch api for update bio data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -26,6 +27,13 @@ async function run() {
 
     const userCollections = client.db('matrimonyDB').collection('users')
 
+    // post json web token
+    app.post('/jwt', async(req, res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1hr'});
+        res.send({token})
+      })
+
     app.post('/users', async(req, res)=>{
         const user = req.body;
         const query = {email: user.email}
@@ -36,6 +44,44 @@ async function run() {
         const result = await userCollections.insertOne(user)
         res.send(result)
       })
+
+    app.get('/users/:email', async(req, res)=>{
+        const email = req.params.email;
+        const query = {email: email}
+        const result = await userCollections.findOne(query)
+        res.send(result)
+    })
+
+    app.patch('/users/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const options = { upsert: true };
+        const updateUser = req.body;
+        const user = {
+            $set:{
+                age: updateUser.age,
+                dof: updateUser.dof,
+                email: updateUser.email,
+                fatherName: updateUser.fatherName,
+                gender: updateUser.gender,
+                height: updateUser.height,
+                motherName: updateUser.motherName,
+                name: updateUser.name,
+                occupation: updateUser.occupation,
+                partnerAge: updateUser.partnerAge,
+                partnerHeight: updateUser.partnerHeight,
+                partnerWeight: updateUser.partnerWeight,
+                permanentDivision: updateUser.permanentDivision,
+                phone: updateUser.phone,
+                photo: updateUser.photo,
+                presentDivision: updateUser.presentDivision,
+                race: updateUser.race,
+                weight: updateUser.weight,
+            }
+        }
+        const result = await userCollections.updateOne(filter, user, options)
+        res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
