9f694628d0350350e3ac2cc9eb8d844213ffdb66
rabby4 <mdrabbybd4747@gmail.com>
2023-11-26 23:38:58 +0600

    create post api for add user in db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v07t2jx.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,6 +23,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const userCollections = client.db('matrimonyDB').collection('users')
+
+    app.post('/users', async(req, res)=>{
+        const user = req.body;
+        const query = {email: user.email}
+        const existUser = await userCollections.findOne(query)
+        if(existUser){
+          return res.send({message: 'user already exist', insertedId: null})
+        }
+        const result = await userCollections.insertOne(user)
+        res.send(result)
+      })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
